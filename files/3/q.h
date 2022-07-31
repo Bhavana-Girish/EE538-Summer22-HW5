410f9e7bbaf97a4e7148e9d96f43a5c233f387e2
Bhavana-Girish <105894473+Bhavana-Girish@users.noreply.github.com>
2022-07-31 16:02:27 -0700

    update q3

--- files/3/q.h
+++ files/3/q.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <unordered_set>
+#include <algorithm>
 
 // Implementing a simple password cracker!
 //-----------------------------------------------------------------------------
